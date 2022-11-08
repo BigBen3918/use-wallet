c0176f1875b147f3c1afb4ebd8e0082bc21d0423
JenisBen <benjamin199551@gmail.com>
2022-11-08 11:33:32 -0800

    update

--- dist/cjs/hooks/walletBalance.d.ts
+++
@@ -1,6 +0,0 @@
-import { Account, EthereumProvider } from '../types';
-export declare function useWalletBalance({ account, ethereum, pollBalanceInterval, }: {
-    account?: Account | null;
-    ethereum?: EthereumProvider;
-    pollBalanceInterval: number;
-}): string;
--- dist/cjs/hooks/watchBlockNumber.d.ts
+++
@@ -1,8 +0,0 @@
-import { EthereumProvider } from '../types';
-export declare function useWatchBlockNumber({ ethereum, pollBlockNumberInterval, }: {
-    ethereum: EthereumProvider;
-    pollBlockNumberInterval: number;
-}): {
-    addBlockNumberListener: (cb: any) => void;
-    removeBlockNumberListener: (cb: any) => void;
-};
